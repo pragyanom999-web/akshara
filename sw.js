1a8d8e210490395d486381160c94f17f3d9df75a
pragyanom999-web <pragyanom999@gmail.com>
2026-09-10 11:16:27 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,22 @@
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open('akshara-cache-v1').then((cache) => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/script.js',
+        '/content.json',
+        '/logo.png'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});

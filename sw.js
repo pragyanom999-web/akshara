da1e2198002d4f81c2388523c73378d963e772f3
pragyanom999-web <pragyanom999@gmail.com>
2026-09-10 11:32:20 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open('akshara-cache-v1').then((cache) => {
+    caches.open('akshara-cache-v2').then((cache) => {
       return cache.addAll([
         '/',
         '/index.html',
